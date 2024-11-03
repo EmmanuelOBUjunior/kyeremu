2d57ba9e5ccf67e71fc8b27f4d680ee0b242d9d0
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 02:12:30 +0000

    Added a home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex-1 bg-red-50 p-4 flex flex-col justify-center">Testing</main>
-  )
+    <main className="flex-1 bg-red-50 p-4 flex flex-col justify-center">
+      Testing
+    </main>
+  );
 }
