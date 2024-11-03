f5526ad5b95b1c303f497233779cbf1f8d8d1fff
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 01:56:47 +0000

    working on the layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ function App() {
 
   return (
     <div className="flex flex-col p-4">
-      <h1 className='text-blue-800'>Welcome to Kyeremu</h1>
+      <section className="min-h-screen flex flex-col">
+
+      </section>
     </div>
   )
 }
