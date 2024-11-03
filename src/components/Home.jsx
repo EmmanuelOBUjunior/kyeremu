e74a3ac975feda8b4f6dc78a39e41f69c6a2a04b
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 14:16:28 +0000

    Working on the logic to record audio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 export default function Home({ setFile, setAudioStream }) {
+    
+
+
   return (
     <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center text-center pb-20">
       <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl">
