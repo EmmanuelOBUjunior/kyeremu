2b48ebcd059efeeab229c996bcd395a498f16aa5
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 03:51:48 +0000

    Working on the display file component

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -1,6 +1,6 @@
 
 export default function FileDisplay({file, audioStream, handleAudioReset}) {
   return (
-    <div>FileDisplay</div>
+    <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center text-center pb-20"></main>
   )
 }
