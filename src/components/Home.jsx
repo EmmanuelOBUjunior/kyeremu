7f8f9faba0cd0776e5ea21da9bfd3a9d2a0c2b57
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 02:32:35 +0000

    Working on the homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center">
+    <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center text-center">
       <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl">
         Kyere<span className="text-blue-400 bold">Mu</span>
       </h1>
-      <h3>
+      <h3 className="font-medium md:text-lg">
         Record{" "}
         <span className="text-blue-400 mx-1">
           <i class="fa-solid fa-arrow-right"></i>
