07b8b4332aba74a364097f56df1f2ee63297a4e0
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 02:13:59 +0000

    Added nav component

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,7 @@ function App() {
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
-        <header className="flex items-center justify-between gap-4 p-4">
-          <h1>
-            Kyere<span className="text-blue-400">Mu</span>
-          </h1>
-          <button className="flex items-center gap-2">
-            <p>New</p>
-            <i class="fa-solid fa-plus"></i>
-          </button>
-        </header>
+       
         <Home/>
       </section>
       <footer></footer>
