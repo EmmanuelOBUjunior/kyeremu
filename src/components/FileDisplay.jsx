3e678f9a50028015b51bba8648338b3c4371a942
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 14:10:05 +0000

    Working on the display file component

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -5,12 +5,12 @@ export default function FileDisplay({ file, audioStream, handleAudioReset }) {
       <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl">
         Your<span className="text-blue-400 bold"> File</span>
       </h1>
-      <div className="mx-auto flex text-left flex-col">
+      <div className="mx-auto flex text-left flex-col my-4">
         <h3 className="font-semibold">Name</h3>
         <p>{file.name}</p>
       </div>
       <div className="flex items-center justify-between gap-4">
-        <button className="text-slate-400">Reset</button>
+        <button className="text-slate-400 hover:text-blue-500 duration-200">Reset</button>
         <button className="specialBtn px-4 py-2 rounded-lg text-blue-400"><p>Transcribe</p></button>
       </div>
     </main>
