8f0acb971e9afe9ac478c4cc0386c8f2af5e3113
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 02:15:08 +0000

    Import nav into app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Home from "./components/Home";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
-       
+       <Nav/>
         <Home/>
       </section>
       <footer></footer>
