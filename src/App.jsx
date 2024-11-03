79e1c4b4b5c15f49108b376d4b1a3c107ecee6b5
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 02:08:56 +0000

    working on the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className="flex flex-col p-4 max-w-[1000px] mx-auto w-full">
+    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
-        <header className="flex items-center justify-between gap-4">
+        <header className="flex items-center justify-between gap-4 p-4">
           <h1>
             Kyere<span className="text-blue-400">Mu</span>
           </h1>
@@ -11,7 +11,7 @@ function App() {
             <i class="fa-solid fa-plus"></i>
           </button>
         </header>
-        <main className="flex-1"></main>
+        <main className="flex-1 bg-red-50 p-4 justify-center"> Testitng</main>
       </section>
       <footer></footer>
     </div>
