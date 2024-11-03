2c11250572c01bf3e6e5c57fbd0727fe19226f3a
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 02:20:16 +0000

    Working on the homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex-1 bg-red-50 p-4 flex flex-col justify-center">
-      <h1>Kyere<span>Mu</span></h1>
+    <main className="flex-1 p-4 flex flex-col justify-center">
+      <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl">Kyere<span>Mu</span></h1>
     </main>
   );
 }
