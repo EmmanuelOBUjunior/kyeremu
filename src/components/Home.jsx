198e67e67911509b8f8b3d37965310a03cd41075
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 02:24:10 +0000

    Working on the homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ export default function Home() {
       <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl">
         Kyere<span className="text-blue-400 bold">Mu</span>
       </h1>
+      <h3>Record<i class="fa-solid fa-arrow-right"></i>Transcribe<i class="fa-solid fa-arrow-right"></i>Translate</h3>
     </main>
   );
 }
