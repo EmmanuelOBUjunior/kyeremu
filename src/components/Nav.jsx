825a781099da9ff90cb807adb8b30f1db09d7b0c
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 02:21:45 +0000

    Working on the homepage

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,8 @@
 export default function Nav() {
   return (
     <nav className="flex items-center justify-between gap-4 p-4">
-    <h1>
-      Kyere<span className="text-blue-400">Mu</span>
+    <h1 className="font-medium">
+      Kyere<span className="text-blue-400 bold">Mu</span>
     </h1>
     <button className="flex items-center gap-2">
       <p>New</p>
