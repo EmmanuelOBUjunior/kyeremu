6ea6f607b317f96d032a47db1c497a6c10a40a59
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 03:53:48 +0000

    Working on the display file component

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -3,8 +3,9 @@ export default function FileDisplay({file, audioStream, handleAudioReset}) {
   return (
     <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center text-center pb-20">
         <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl">
-        Your<span className="text-blue-400 bold">File</span>
+        Your<span className="text-blue-400 bold"> File</span>
       </h1>
+      <div className="flex items-center gap-2"></div>
     </main>
   )
 }
