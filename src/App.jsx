da5da5da6f386fb2004c56335b8200c5315c9303
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 01:59:36 +0000

    working on the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-
 function App() {
-
   return (
     <div className="flex flex-col p-4">
       <section className="min-h-screen flex flex-col">
         <header>
-          <h1>Kyere<span className="text-blue-400">Mu</span></h1>
-          <button><p>New</p></button>
+          <h1>
+            Kyere<span className="text-blue-400">Mu</span>
+          </h1>
+          <button>
+            <p>New</p>
+          </button>
         </header>
-        <main>
-
-        </main>
+        <main></main>
       </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
