a0d693899f8e902dfab9bdd627d5490554332f2b
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 02:25:07 +0000

    Working on the homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex-1 p-4 flex flex-col justify-center">
+    <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center">
       <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl">
         Kyere<span className="text-blue-400 bold">Mu</span>
       </h1>
-      <h3>Record<i class="fa-solid fa-arrow-right"></i>Transcribe<i class="fa-solid fa-arrow-right"></i>Translate</h3>
+      <h3>Record</h3>
     </main>
   );
 }
