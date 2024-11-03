00175c5d9156cfef44e73a36a10dae6e41205717
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 02:14:52 +0000

    Import nav into app file

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,14 @@
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <nav className="flex items-center justify-between gap-4 p-4">
+    <h1>
+      Kyere<span className="text-blue-400">Mu</span>
+    </h1>
+    <button className="flex items-center gap-2">
+      <p>New</p>
+      <i class="fa-solid fa-plus"></i>
+    </button>
+  </nav>
   )
 }
