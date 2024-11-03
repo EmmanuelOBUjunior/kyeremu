83b4034b8f9c27384fa51fbc363e33032ad5d245
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-03 03:38:39 +0000

    Working on a conditional check to render filedisplay when a file has been uploaded or an audiostream can be found

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
+import FileDisplay from "./components/FileDisplay";
 
 function App() {
+  const [file, setFile] = useState(null)
+  const [audioStream, setAudioStream] = useState(null)
+
+  const isAvailable = file || audioStream
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
        <Nav/>
-        <Home/>
+        {isAvailable ? <FileDisplay/> : <Home/>}
       </section>
       <Footer/>
     </div>
