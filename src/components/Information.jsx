0f5117913883521c50ab61237b750689492ec362
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 16:10:07 +0000

    styling: working on the style for active tab

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 export default function Information() {
-    const [tab, setTab] = useState('transcription')
+    const [tab, setTab] = useState('translation')
   return (
     <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center text-center pb-20 mx-auto w-full max-w-prose">
       <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl whitespace-nowrap">
@@ -9,7 +9,7 @@ export default function Information() {
       </h1>
         <div className="flex mx-auto bg-white  shadow rounded-full overflow-hidden items center gap-2">
             <button className={"px-4 py-1 font-medium "+ (tab === 'transcription' ? 'bg-blue-500 text-white' : ' text-blue-400 hover:text-blue-600')}>Transcription</button>
-            <button className={"px-4 py-1 font-medium" + (tab === 'translation' ? 'bg-blue-500 text-white': ' text-blue-400 hover:text-blue-600')}>Translation</button>
+            <button className={"px-4 py-1 font-medium" + (tab === 'translation' ? ' bg-blue-500 text-white': ' text-blue-400 hover:text-blue-600')}>Translation</button>
         </div>
       </main>
   )
