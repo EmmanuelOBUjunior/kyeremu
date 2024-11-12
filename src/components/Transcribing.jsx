59326c4582e97043acd260564491d353c00bf268
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 15:12:02 +0000

    Working on the transcribing component

--- src/components/Transcribing.jsx
+++ src/components/Transcribing.jsx
@@ -9,7 +9,13 @@ export default function Transcribing() {
             <p>{!downloading ? 'warming up cylinders' : 'core cylinders engaged'}</p>
         </div>
         <div className="flex flex-col gap-2 sm:gap-4 max-w-[500px] mx-auto w-full">
-            {}
+            {[0,1,2].map(val=>{
+                return(
+                    <div className={"rounded-full h-2 sm:h-3 bg-slate-400 loading " + `loading${val}`}>
+                        
+                    </div>
+                )
+            })}
         </div>
     </div>
   )
