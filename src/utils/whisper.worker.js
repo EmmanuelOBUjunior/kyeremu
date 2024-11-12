aa50569ccdd8d7538fff7ed3518988fb69ea3472
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 18:01:33 +0000

    Creating a  class for transcription pipeline

--- src/utils/whisper.worker.js
+++ src/utils/whisper.worker.js
@@ -8,7 +8,7 @@ class MyTranscriptionPipeline {
   static async getInstance(progress_callback = null) {
     if(this.instance === null){
         this.instance = await pipeline(this.task, null, {progress_callback})
-        return this.instance
     }
+    return this.instance
   }
 }
