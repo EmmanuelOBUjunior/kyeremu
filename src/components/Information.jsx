c37388fec592e0ad10f95c5add513076339b58fd
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 15:54:36 +0000

    Working on the information component

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,7 +1,7 @@
 
 export default function Information() {
   return (
-    <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center text-center pb-20 mx-auto max-w-full">
+    <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center text-center pb-20 mx-auto w-full max-w-prose">
       <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl whitespace-nowrap">
         Your<span className="text-blue-400 bold"> Transcription</span>
       </h1>
