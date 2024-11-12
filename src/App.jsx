80c1754de518224884051d5283d68a27785a20f9
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 17:31:53 +0000

    Working on the handleformsubmission logic

--- src/App.jsx
+++ src/App.jsx
@@ -62,6 +62,10 @@ function App() {
     return audio
   }
 
+  async function handleFormSubmission() {
+    
+  }
+
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
