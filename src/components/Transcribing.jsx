238a3fa9915c5c64ccf6ef217408444b1d93ab37
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 15:12:54 +0000

    Working on the transcribing component

--- src/components/Transcribing.jsx
+++ src/components/Transcribing.jsx
@@ -1,4 +1,4 @@
-export default function Transcribing() {
+export default function Transcribing({downloading}) {
   return (
     <div className="flex items-center flex-col justify-center gap-10 md-gap-14 py-14">
       <div className="flex flex-col gap-2 sm:gap-4">
