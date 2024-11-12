79fd80b2248178327cb3228669c2549f7a940589
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 17:06:01 +0000

    feat: rendered transcription and translation conditionally using the tab

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,6 @@ function App() {
   }
   const isAudioAvailable = file || audioStream;
 
-
-
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
