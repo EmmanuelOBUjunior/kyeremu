e975f7b2a0c304317e0343ac1d466db793a67075
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 19:30:52 +0000

    bug: fixing whisper model bug

--- src/utils/whisper.worker.js
+++ src/utils/whisper.worker.js
@@ -3,7 +3,7 @@ import { MessageTypes } from "./presets";
 
 class MyTranscriptionPipeline {
   static task = "automatic-speech-recognition";
-  static model = "openai/whisper-tiny.en";
+  static model = 'openai/whisper-tiny.en'
   static instance = null;
 
   static async getInstance(progress_callback = null) {
