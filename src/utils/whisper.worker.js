b73cb6b195735eead5f6239cc4b289000453000c
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 17:59:13 +0000

    Creating a  class for transcription pipeline

--- src/utils/whisper.worker.js
+++ src/utils/whisper.worker.js
@@ -4,4 +4,5 @@ import { pipeline } from "@xenova/transformers";
 class MyTranscriptionPipeline{
     static task = 'automatic-speech-recognition'
     static model = 'openai/whisper-tiny.en'
+    static instance = null
 }
