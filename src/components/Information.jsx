608a33b4466ce16aaf7afd6f50996c511fcc6590
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 16:11:05 +0000

    styling: working on the style for active tab

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 export default function Information() {
-    const [tab, setTab] = useState('transcription')
+    const [tab, setTab] = useState('translation')
   return (
     <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center text-center pb-20 mx-auto w-full max-w-prose">
       <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl whitespace-nowrap">
