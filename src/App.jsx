cc7a4b23f46d8f923ef3691fedb5b0aef3e383ce
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 17:33:44 +0000

    Working on the handleformsubmission logic

--- src/App.jsx
+++ src/App.jsx
@@ -65,6 +65,7 @@ function App() {
   async function handleFormSubmission() {
     if (!file && audioStream) return;
     let audio = await readAudiofrom(file ? file : audioStream)
+    const model_name =  `openai/whisper-tiny.en`
   }
 
   return (
