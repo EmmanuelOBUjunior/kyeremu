8aa4e6cc4c903eded35879138bd9a2fd5ca47a15
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 15:40:17 +0000

    Working on the page structure of kyeremu

--- src/components/Transcribing.jsx
+++ src/components/Transcribing.jsx
@@ -1,6 +1,6 @@
 export default function Transcribing({downloading}) {
   return (
-    <div className="flex items-center flex-col justify-center gap-10 md-gap-14 py-14">
+    <div className="flex items-center flex-col justify-center gap-10 md-gap-14 pb-14 flex-1">
       <div className="flex flex-col gap-2 sm:gap-4">
         <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
           <span className="text-blue-400 bold">Transcribing</span>
