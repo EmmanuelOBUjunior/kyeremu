5c61a4383bb39c29ab1a601e3dd5c0212191f746
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 15:50:11 +0000

    Working on the information component

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,6 +1,10 @@
 
 export default function Information() {
   return (
-    <div>Information</div>
+    <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center text-center pb-20 w-72 sm:w-96 mx-auto max-w-full">
+      <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl">
+        Your<span className="text-blue-400 bold"> File</span>
+      </h1>
+      </main>
   )
 }
