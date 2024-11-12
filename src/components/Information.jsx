bb2da4891156bafc06d741a45e420a132616adf3
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-12 16:04:06 +0000

    styling: working on the style for active tab

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -8,7 +8,7 @@ export default function Information() {
         Your<span className="text-blue-400 bold"> Transcription</span>
       </h1>
         <div className="flex mx-auto bg-white border-2 border-solid border-blue-300 shadow rounded-full overflow-hidden items center gap-2">
-            <button className="px-4 py-1 font-medium">Transcription</button>
+            <button className={"px-4 py-1 font-medium "+ (tab === 'transcription' ? 'bg-blue-500 text-white': '  text-blue-400 hover:text-blue-600')}>Transcription</button>
             <button className="px-4 py-1 font-medium">Translation</button>
         </div>
       </main>
