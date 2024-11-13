e5bae463112f370b12fb5c2da899a43ddabe881e
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-13 19:26:40 +0000

    bug: fixing whisper model bug

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    headers:{
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    }
+  },
   optimizeDeps:{
     exclude: ['@xenova/transformers']
   },
