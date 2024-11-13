75460515aff8768a521b927994bc4b7a1589abea
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-13 22:18:04 +0000

    bug: fixing whisper model bug

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -1,5 +1,5 @@
 export default function FileDisplay({ file, audioStream, handleAudioReset, handleFormSubmission }) {
-  console.log("File:", file);
+  console.log("File:", file, audioStream);
   return (
     <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center text-center pb-20 w-72 sm:w-96 mx-auto max-w-full">
       <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl">
