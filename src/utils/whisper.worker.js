3e7c369b22b7f02b5d679a0d73f8e1f10e25b8f0
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-13 18:55:36 +0000

    bug: fixing whisper model bug

--- src/utils/whisper.worker.js
+++ src/utils/whisper.worker.js
@@ -8,7 +8,7 @@ class MyTranscriptionPipeline {
 
     static async getInstance(progress_callback = null) {
         if (this.instance === null) {
-            this.instance = await pipeline(this.task, null, { progress_callback })
+            this.instance = await pipeline(this.task, this.model, { progress_callback })
         }
 
         return this.instance
