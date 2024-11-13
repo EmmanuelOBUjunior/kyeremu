faf06a6f3d502bb9ee8366885f60a8db4b06dd27
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-13 18:52:13 +0000

    bug: fixing whisper model bug

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,104 +1,88 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState, useEffect, useRef } from 'react'
 
-export default function Home({ setFile, setAudioStream }) {
-  const [recordingStatus, setRecordingStatus] = useState("inactive");
-  const [audioChunks, setAudioChunks] = useState([]);
-  const [duration, setDuration] = useState(0);
-  const mediaRecoder = useRef(null);
-  const mimeType = "audio/webm";
+export default function Home(props) {
+    const { setAudioStream, setFile } = props
 
-  useEffect(() => {
-    if (recordingStatus === "inactive") return;
-    const interval = setInterval(() => {
-      setDuration((curr) => curr + 1);
-    }, 1000);
-    return () => clearInterval(interval);
-  });
+    const [recordingStatus, setRecordingStatus] = useState('inactive')
+    const [audioChunks, setAudioChunks] = useState([])
+    const [duration, setDuration] = useState(0)
 
-  async function startRecording() {
-    let tempStream;
-    console.log("Start Recording");
-    try {
-      const streamData = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: false,
-      });
-      tempStream = streamData;
-    } catch (error) {
-      console.log(err.message);
-      return;
+    const mediaRecorder = useRef(null)
+
+    const mimeType = 'audio/webm'
+
+    async function startRecording() {
+        let tempStream
+        console.log('Start recording')
+
+        try {
+            const streamData = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+                video: false
+            })
+            tempStream = streamData
+        } catch (err) {
+            console.log(err.message)
+            return
+        }
+        setRecordingStatus('recording')
+
+        //create new Media recorder instance using the stream
+        const media = new MediaRecorder(tempStream, { type: mimeType })
+        mediaRecorder.current = media
+
+        mediaRecorder.current.start()
+        let localAudioChunks = []
+        mediaRecorder.current.ondataavailable = (event) => {
+            if (typeof event.data === 'undefined') { return }
+            if (event.data.size === 0) { return }
+            localAudioChunks.push(event.data)
+        }
+        setAudioChunks(localAudioChunks)
     }
-    setRecordingStatus("recording");
 
-    //create new media recorder instance using the stream
-    const media = new MediaRecorder(tempStream, { type: mimeType });
-    mediaRecoder.current = media;
+    async function stopRecording() {
+        setRecordingStatus('inactive')
+        console.log('Stop recording')
 
-    mediaRecoder.current.start();
-    let localAudioChunks = [];
-    mediaRecoder.current.ondataavailable = (event) => {
-      if (typeof event.data === "undefined") return;
-      if (event.data.size === 0) return;
-      localAudioChunks.push(event.data);
-    };
-    setAudioChunks(localAudioChunks);
-  }
+        mediaRecorder.current.stop()
+        mediaRecorder.current.onstop = () => {
+            const audioBlob = new Blob(audioChunks, { type: mimeType })
+            setAudioStream(audioBlob)
+            setAudioChunks([])
+            setDuration(0)
+        }
+    }
 
-  async function stopRecording() {
-    setRecordingStatus("inactive");
-    console.log("Stop recording");
-    mediaRecoder.current.stop();
-    mediaRecoder.current.onstop = () => {
-      let audioBlob = new Blob(audioChunks, { type: mimeType });
-      setAudioStream(audioBlob);
-      setAudioChunks([]);
-      setDuration(0);
-    };
-  }
+    useEffect(() => {
+        if (recordingStatus === 'inactive') { return }
+
+        const interval = setInterval(() => {
+            setDuration(curr => curr + 1)
+        }, 1000)
+
+        return () => clearInterval(interval)
+    })
 
 
-
-  return (
-    <main className="flex-1 p-4 gap-3 sm:gap-4 md:gap-5 flex flex-col justify-center text-center pb-20">
-      <h1 className="font-bold text-5xl sm:text-6xl md:text-7xl">
-        Kyere<span className="text-blue-400 bold">Mu</span>
-      </h1>
-      <h3 className="font-medium md:text-lg">
-        Record{" "}
-        <span className="text-blue-400 mx-1">
-          <i className="fa-solid fa-arrow-right"></i>
-        </span>{" "}
-        Transcribe{" "}
-        <span className="text-blue-400 mx-1">
-          <i className="fa-solid fa-arrow-right"></i>
-        </span>{" "}
-        Translate
-      </h3>
-      <button onClick={recordingStatus === 'recording' ? stopRecording : startRecording} className="specialBtn px-4 py-2 rounded-xl flex items-center text-base justify-between gap-4 mx-auto max-w-full w-72 my-4">
-        <p className="text-blue-400">
-          {recordingStatus === "inactive" ? "Record" : `Stop recording`}
-        </p>
-        <div className="flex items-center gap-2">
-          {duration !== 0 && <p className="text-sm">{duration}s</p>}
-        <i className={"fa-solid duration-200 fa-microphone" + (recordingStatus === 'recording'? ' text-rose-500' : "")}></i>
-        </div>
-      </button>
-      <p className="text-base">
-        Or{" "}
-        <label className="text-blue-400 cursor-pointer hover:text-blue-600 duration-200">
-          upload
-          <input
-            accept=".mp3,.wave"
-            type="file"
-            className="hidden"
-            onChange={(e) => {
-              setFile(e.target.files[0]);
-            }}
-          />
-        </label>{" "}
-        a mp3 file
-      </p>
-      <p className="text-slate-400 italic">Free Now...Free Forever</p>
-    </main>
-  );
+    return (
+        <main className='flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4  justify-center pb-20'>
+            <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Free<span className='text-blue-400 bold'>Scribe</span></h1>
+            <h3 className='font-medium md:text-lg'>Record <span className='text-blue-400'>&rarr;</span> Transcribe <span className='text-blue-400'>&rarr;</span> Translate</h3>
+            <button onClick={recordingStatus === 'recording' ? stopRecording : startRecording} className='flex specialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
+                <p className='text-blue-400'>{recordingStatus === 'inactive' ? 'Record' : `Stop recording`}</p>
+                <div className='flex items-center gap-2'>
+                    {/* {duration !== 0 && (
+                        <p className='text-sm'>{duration}s</p>
+                    )} */}
+                    <i className={"fa-solid duration-200 fa-microphone " + (recordingStatus === 'recording' ? ' text-rose-300' : "")}></i>
+                </div>
+            </button>
+            <p className='text-base'>Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>upload <input onChange={(e) => {
+                const tempFile = e.target.files[0]
+                setFile(tempFile)
+            }} className='hidden' type='file' accept='.mp3,.wave' /></label> a mp3 file</p>
+            <p className='italic text-slate-400'>Free now free forever</p>
+        </main>
+    )
 }
